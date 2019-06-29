a458ba4596dd1dc8d1a102c96fad42ac9c0c118e
leejinseong <dlwls5201@naver.com>
2019-06-29 22:31:57 +0900

    change Detail Activity name

---
+++ app/src/main/java/com/mashup/molink/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.mashup.molink
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
